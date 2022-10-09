69e9a91481ad3dbe8c31528e308f12f997d63d4d
Reid Lawson <rldc6@umsystem.edu>
2022-10-09 15:55:23 -0500

    Cleaned up code with better variable names

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,8 +1,8 @@
 import itertools as itt
 
-f = itt.cycle([""] * 2 + ["Fizz"])
-b = itt.cycle([""] * 4 + ["Buzz"])
-fb = (fizz + buzz for fizz, buzz in zip(f, b))
-result = (word or n for word, n in zip(fb, itt.count(1)))
+isFizz = itt.cycle([""] * 2 + ["Fizz"])
+isBuzz = itt.cycle([""] * 4 + ["Buzz"])
+isFizzBuzz = (fizz + buzz for fizz, buzz in zip(isFizz, isBuzz))
+result = (word or n for word, n in zip(isFizzBuzz, itt.count(1)))
 for i in itt.islice(result, 100):
     print(i)
