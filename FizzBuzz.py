1fd86555753e455b244f1602186388bbeddc9c0d
Reid Lawson <54284239+rlawson8@users.noreply.github.com>
2022-10-11 16:59:44 -0500

    Delete FizzBuzz.py

--- FizzBuzz.py
+++
@@ -1,8 +0,0 @@
-import itertools as itt
-
-isFizz = itt.cycle([""] * 2 + ["Fizz"])
-isBuzz = itt.cycle([""] * 4 + ["Buzz"])
-isFizzBuzz = (fizz + buzz for fizz, buzz in zip(isFizz, isBuzz))
-result = (word or n for word, n in zip(isFizzBuzz, itt.count(1)))
-for i in itt.islice(result, 100):
-    print(i)
