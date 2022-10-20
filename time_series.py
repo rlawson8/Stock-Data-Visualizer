67a87c9d5dc9fd97eab32a36e0fe226d2228cbae
Caelen Walker <60584145+caelenwalker@users.noreply.github.com>
2022-10-20 15:57:34 -0500

    Add files via upload

---
+++ time_series.py
@@ -0,0 +1,6 @@
+time_series = int(input("Enter time series option (1, 2, 3, 4): "))
+
+if time_series < 0:
+    print("Error: Please enter a value within the range.")
+elif time_series > 4:
+        print("Error: Please enter a value within the range.")
