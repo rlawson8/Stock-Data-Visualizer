4097e72627a2c9e84dd346e6d291bf1c5cb3c95c
Caelen Walker <60584145+caelenwalker@users.noreply.github.com>
2022-10-20 16:13:37 -0500

    Update time_series.py

--- time_series.py
+++ time_series.py
@@ -1,4 +1,10 @@
-time_series = int(input("Enter time series option (1, 2, 3, 4): "))
+print("Select the Time Series of the chart you want to Generate: ")
+print("1. Intraday")
+print("2. Daily")
+print("3. Weekly")
+print("4. Monthly")
+
+time_series = int(input("\nEnter time series option (1, 2, 3, 4): "))
 
 if time_series < 0:
     print("Error: Please enter a value within the range.")
